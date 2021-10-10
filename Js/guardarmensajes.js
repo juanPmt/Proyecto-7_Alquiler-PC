c7c582b351962081c8a8b157005dd04a926d46e9
Eusebio Buelvas <eusebio.buelvas.mt@correo.usa.co>
2021-10-10 15:51:11 -0500

    Se crea actualizaciones mensajes

---
+++ Js/guardarmensajes.js
@@ -0,0 +1,23 @@
+function guardar(){
+    let mensaje = {
+        id: +$("#id").val(),
+        messajetext:  $("#messajetext").val(),
+    };
+
+    console.log("Se va a registrar el equipo", mensaje);
+
+    $.ajax({
+        url: "https://ga61db958975b8f-alquilerpc.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/client/client",
+        type: 'POST',
+        dataType: 'json',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        data: JSON.stringify(mensaje),
+        statusCode:{
+            201:function(){
+                alert('El mensaje es: ');
+            }
+        },
+    });
+}
